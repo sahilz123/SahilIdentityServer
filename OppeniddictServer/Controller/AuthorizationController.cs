ee8dada5a85928cbc96018f01873f4a33acc138f
sahilz123 <157479424+sahilz123@users.noreply.github.com>
2025-01-31 12:40:13 +0530

    dividing AuthController function into smaller functions

--- OppeniddictServer/Controller/AuthorizationController.cs
+++ OppeniddictServer/Controller/AuthorizationController.cs
@@ -13,10 +13,15 @@ using OppeniddictServer.Constants;
 using Microsoft.AspNetCore.Identity;
 using OppeniddictServer.Identity;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Primitives;
+using System.Data;
+using static System.Net.Mime.MediaTypeNames;
+using System.Security.Principal;
+using System.Net;
 namespace OppeniddictServer.Controller
 {
     [ApiController]
-    public class AuthorizationController :Microsoft.AspNetCore.Mvc.Controller
+    public class AuthorizationController : Microsoft.AspNetCore.Mvc.Controller
     {
         private readonly IOpenIddictApplicationManager _applicationManager;
         private readonly IOpenIddictAuthorizationManager _authorizationManager;
@@ -30,7 +35,7 @@ namespace OppeniddictServer.Controller
         public AuthorizationController(
             IOpenIddictApplicationManager applicationManager,
             IOpenIddictAuthorizationManager authorizationManager,
-            IOpenIddictScopeManager scopeManager, 
+            IOpenIddictScopeManager scopeManager,
             AuthService authService,
             UserManager<UserIdentity> userManager,
             RoleManager<UserIdentityRole> roleManager
@@ -69,15 +74,14 @@ namespace OppeniddictServer.Controller
         [HttpPost("~/connect/authorize")]
         [IgnoreAntiforgeryToken]
         public async Task<IActionResult> Authorize()
-            
+
         {
             var request = HttpContext.GetOpenIddictServerRequest() ??
                 throw new InvalidOperationException(Error.OpenIdException);
 
-            //var result1 =await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             var result = await HttpContext.AuthenticateAsync(IdentityConstants.ApplicationScheme);
 
-            var isAuthenticated=_authService.IsAuthenticated(result, request);
+            var isAuthenticated = _authService.IsAuthenticated(result, request);
 
 
             var parameters = _authService.ParseOAuthParameters(HttpContext);
@@ -87,24 +91,63 @@ namespace OppeniddictServer.Controller
                     authenticationSchemes: CookieAuthenticationDefaults.AuthenticationScheme,
                    properties: new AuthenticationProperties
                    {
-                       RedirectUri = _authService.BuilderRediect(HttpContext.Request,parameters)
+                       RedirectUri = _authService.BuilderRediect(HttpContext.Request, parameters)
                    });
             }
-            
-            var application = await _applicationManager.FindByClientIdAsync(request.ClientId!) ??
-                throw new InvalidOperationException(Error.ClientNotFound);
+            else
+            {
+                var application = await _applicationManager.FindByClientIdAsync(request.ClientId!) ??
+                                throw new InvalidOperationException(Error.ClientNotFound);
+
+                string clientId = await _applicationManager.GetIdAsync(application) ?? throw new NullReferenceException();
+
+                CheckConsentClaim(result, parameters);
+
+                var email = result.Principal!.FindFirst(ClaimTypes.Email)!.Value;
+
+                var user = await _userManager.FindByEmailAsync(email);
+                var claims = await _userManager.GetClaimsAsync(user);
+
+
+
+                var roles = result.Principal.FindAll(ClaimTypes.Role)
+                                            .Select(r => r.Value)
+                                            .ToImmutableArray();
+
+                IList<Claim> roleclaims = GetRoleClaims(result).Result;
+                string subject = result.Principal.FindFirst(ClaimTypes.Email)!.Value;
+
+                ClaimsIdentity identity = SetIdentity(subject, email, roles, user.UserName, claims, roleclaims);
+                identity.SetScopes(request.GetScopes());
+
+                identity.SetResources(await _scopeManager.ListResourcesAsync(identity.GetScopes()).ToListAsync());
+
+
+                var authorization = GetAuthorization(clientId, subject, identity).Result;
+
+                identity.SetAuthorizationId(await _authorizationManager.GetIdAsync(authorization));
+                identity.SetDestinations(AuthService.GetDestination);
+
+                return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+
+            }
+            /*var application = await _applicationManager.FindByClientIdAsync(request.ClientId!) ??
+                                                throw new InvalidOperationException(Error.ClientNotFound);
+
+
 
             var consentclaim = result.Principal!.GetClaim(Constant.ConsentNaming);
 
             if (consentclaim != Constant.GrantAccessValue)
             {
                 var returnUrl = HttpUtility.UrlEncode(_authService.BuilderRediect(HttpContext.Request, parameters));
-                var consentRedirectUrl =Urls.ConsentWithReturnUrl+returnUrl;
+                var consentRedirectUrl = Urls.ConsentWithReturnUrl + returnUrl;
 
                 return Redirect(consentRedirectUrl);
-            }
+            }*/
 
-            var email = result.Principal!.FindFirst(ClaimTypes.Email)!.Value;
+            /*var email = result.Principal!.FindFirst(ClaimTypes.Email)!.Value;
 
             var user = await _userManager.FindByEmailAsync(email);
             var claims = await _userManager.GetClaimsAsync(user);
@@ -114,9 +157,9 @@ namespace OppeniddictServer.Controller
             var roles = result.Principal.FindAll(ClaimTypes.Role)
                                         .Select(r => r.Value)
                                         .ToImmutableArray();
-            IList<Claim> roleclaims = new List<Claim>();
+            IList<Claim> roleclaims = new List<Claim>();*/
 
-            foreach (var role in roles)
+            /*foreach (var role in roles)
             {
                 var roleObject = await _roleManager.FindByNameAsync(role);
                 if (roleObject == null)
@@ -132,11 +175,9 @@ namespace OppeniddictServer.Controller
                         roleclaims.Add(c); 
                     }
                 }
-            }
+            }*/
 
-
-
-            string _subject = result.Principal.FindFirst(ClaimTypes.Email)!.Value;
+            /*string _subject = result.Principal.FindFirst(ClaimTypes.Email)!.Value;
             var identity = new ClaimsIdentity(
             authenticationType: TokenValidationParameters.DefaultAuthenticationType,
             nameType: Claims.Name,
@@ -146,46 +187,141 @@ namespace OppeniddictServer.Controller
                     .SetClaim(Claims.Email, email)
                     .SetClaims(Claims.Role, roles)
                     .SetClaim(Claims.PreferredUsername, user.UserName)
-            ;
+            ;*/
 
-            foreach(var c in claims)
+            /*foreach (var c in claims)
             {
-                identity.SetClaim(c.Type,c.Value);
+                identity.SetClaim(c.Type, c.Value);
             }
-            
-            foreach(var c in roleclaims)
+
+            foreach (var c in roleclaims)
             {
-                identity.SetClaim(c.Type,c.Value); 
+                identity.SetClaim(c.Type, c.Value);
             }
-            
+
 
             identity.SetScopes(request.GetScopes());
 
             identity.SetResources(await _scopeManager.ListResourcesAsync(identity.GetScopes()).ToListAsync());
+            */
 
-            string _client= await _applicationManager.GetIdAsync(application)?? throw new NullReferenceException();
-             var authorizations = await _authorizationManager
-                .FindAsync(   
-                subject: _subject,
-                client: _client,
-                status:Statuses.Valid,
-                type: AuthorizationTypes.Permanent,
-                scopes: identity.GetScopes()).ToListAsync();
+            /*string _client = await _applicationManager.GetIdAsync(application) ?? throw new NullReferenceException();
+            var authorizations = await _authorizationManager
+               .FindAsync(
+               subject: _subject,
+               client: _client,
+               status: Statuses.Valid,
+               type: AuthorizationTypes.Permanent,
+               scopes: identity.GetScopes()).ToListAsync();
 
-            var authorization=authorizations.LastOrDefault();
+            var authorization = authorizations.LastOrDefault();
 
             authorization ??= await _authorizationManager.CreateAsync(
                 identity: identity,
                 subject: _subject,
-                client: _client, 
+                client: _client,
                 type: AuthorizationTypes.Permanent,
-                scopes: identity.GetScopes()) ;
+                scopes: identity.GetScopes());
 
             identity.SetAuthorizationId(await _authorizationManager.GetIdAsync(authorization));
             identity.SetDestinations(AuthService.GetDestination);
 
             return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
-            
+            */
+        }
+
+        private async Task<object> GetAuthorization(string clientId, string subject, ClaimsIdentity identity)
+        {
+            var authorizations = await _authorizationManager
+                            .FindAsync(
+                            subject: subject,
+                            client: clientId,
+                            status: Statuses.Valid,
+                            type: AuthorizationTypes.Permanent,
+                            scopes: identity.GetScopes()).ToListAsync();
+
+            var authorization = authorizations.LastOrDefault();
+
+            authorization ??= await _authorizationManager.CreateAsync(
+                identity: identity,
+                subject: subject,
+                client: clientId,
+                type: AuthorizationTypes.Permanent,
+                scopes: identity.GetScopes());
+
+            return authorization;
+        }
+
+        private ClaimsIdentity SetIdentity(string subject, string email,
+                                            ImmutableArray<string> roles,
+                                            string userName,
+                                            IList<Claim> claims,
+                                            IList<Claim> roleclaims)
+        {
+            var identity = new ClaimsIdentity(
+                        authenticationType: TokenValidationParameters.DefaultAuthenticationType,
+                        nameType: Claims.Name,
+                        roleType: Claims.Role);
+
+            identity.SetClaim(Claims.Subject, subject)
+                    .SetClaim(Claims.Email, email)
+                    .SetClaims(Claims.Role, roles)
+                    .SetClaim(Claims.PreferredUsername, userName)
+            ;
+
+            foreach (var c in claims)
+            {
+                identity.SetClaim(c.Type, c.Value);
+            }
+
+            foreach (var c in roleclaims)
+            {
+                identity.SetClaim(c.Type, c.Value);
+            }
+
+            return identity;
+        }
+
+        private async Task<List<Claim>> GetRoleClaims(AuthenticateResult result)
+        {
+
+            var roles = result.Principal!.FindAll(ClaimTypes.Role)
+                                            .Select(r => r.Value)
+                                            .ToImmutableArray();
+
+            IList<Claim> roleclaims = new List<Claim>();
+
+            foreach (var role in roles)
+            {
+                var roleObject = await _roleManager.FindByNameAsync(role);
+                if (roleObject == null)
+                {
+                    continue;
+                }
+
+                var rClaim = await _roleManager.GetClaimsAsync(roleObject);
+                if (rClaim != null && rClaim.Any())
+                {
+                    foreach (var c in rClaim)
+                    {
+                        roleclaims.Add(c);
+                    }
+                }
+            }
+            return roleclaims.ToList();
+        }
+
+        private RedirectResult CheckConsentClaim(AuthenticateResult result, IDictionary<string, StringValues> parameters)
+        {
+
+            if (result.Principal!.GetClaim(Constant.ConsentNaming) != Constant.GrantAccessValue)
+            {
+                var returnUrl = HttpUtility.UrlEncode(_authService.BuilderRediect(HttpContext.Request, parameters));
+                var consentRedirectUrl = Urls.ConsentWithReturnUrl + returnUrl;
+
+                return Redirect(consentRedirectUrl);
+            }
+            return null!;
         }
 
         [HttpPost("~/connect/token")]
@@ -197,40 +333,40 @@ namespace OppeniddictServer.Controller
 
             if (!request.IsAuthorizationCodeGrantType() && !request.IsRefreshTokenGrantType())
                 throw new InvalidOperationException(Error.GrantTypeError);
-            
+
             // Retrieve the claims principal stored in the authorization code/refresh token.
             var result = await HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
             var application = await _applicationManager.FindByClientIdAsync(request.ClientId!);
 
-            var claims = result.Principal!.Claims.ToList();
-                        
-            var email = result.Principal.GetClaim(Claims.Email);              
-            var id = result.Principal.GetClaim(Claims.ClientId);               
-                                                                               
-            var role = result.Principal.GetClaims(Claims.Role);                 
-            var subject = result.Principal.GetClaim(Claims.Email);             
+            var claims = result.Principal!;
+
+            var email = claims.GetClaim(Claims.Email);
+            var id = claims.GetClaim(Claims.ClientId);
+
+            var role = claims.GetClaims(Claims.Role);
+            var subject = claims.GetClaim(Claims.Email);
             var roleList = new List<string> { role.ToString()! }.ToImmutableArray();
 
             if (string.IsNullOrEmpty(email))
-                {
-                    return Forbid(
-                        authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
-                        properties: new AuthenticationProperties(new Dictionary<string, string?>
-                        {
-                            [OpenIddictServerAspNetCoreConstants.Properties.Error] = Errors.InvalidGrant,
-                            [OpenIddictServerAspNetCoreConstants.Properties.ErrorDescription] = Error.TokenInvalid
-                        }));
-                }
+            {
+                return Forbid(
+                    authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
+                    properties: new AuthenticationProperties(new Dictionary<string, string?>
+                    {
+                        [OpenIddictServerAspNetCoreConstants.Properties.Error] = Errors.InvalidGrant,
+                        [OpenIddictServerAspNetCoreConstants.Properties.ErrorDescription] = Error.TokenInvalid
+                    }));
+            }
 
-            var identity = new ClaimsIdentity(  result.Principal.Claims,
+            var identity = new ClaimsIdentity(result.Principal.Claims,
                                                 authenticationType: TokenValidationParameters.DefaultAuthenticationType,
                                                 nameType: Claims.Name,
-                                                roleType: Claims.Role);           
+                                                roleType: Claims.Role);
 
             identity.SetDestinations(AuthService.GetDestination);
 
-                // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
-               return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+            // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
+            return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
 
         }
 
